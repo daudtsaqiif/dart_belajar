5e7883f70293ed8dd80fb2f8c5e2da68423e134f
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-07-23 14:23:18 +0700

    1

---
+++ tugas/5.map.dart
@@ -0,0 +1,47 @@
+void main() {
+  String nama = 'Budi Doraemon Uhuy';
+  int usia = 20;
+  String alamat = 'Jl.Raya No. 1 Jakarta';
+  String email = 'budidoraemonuhuy@gmail.com';
+  int noHp = 08123456789;
+  int totalBelanja = 1000000;
+  bool statusDelivery = true;
+
+  Map sepatu = {
+    'item': 'sepatu',
+    'merek': 'Bandung Cibaduyut',
+    'harga': 300.000
+  };
+
+  Map kaos = {'item': 'kaos', 'merek': 'Arei Gearoutdoor', 'harga': 150000};
+
+  Map celana = {'item': 'Celana', 'merek': 'Arei Gearoutdoor', 'harga': 200000};
+
+  Map tas = {'item': 'Tas', 'merek': 'Gunung Eiger 70L', 'harga': 350000};
+
+  Map jaket = {'item': 'jaket', 'merek': 'Gunung Eiger', 'harga': 200000};
+
+  Map topi = {'item': 'Topi', 'merek': 'Gunung Eiger', 'harga': 100000};
+
+  List daftarBelanja = [
+    sepatu,
+    kaos,
+    celana,
+    tas,
+    jaket,
+    topi
+  ];
+
+  Map dataECommerce = {
+    'nama': nama,
+    'umur': usia,
+    'alamat': alamat,
+    'email': email,
+    'noHp': noHp,
+    'total belanja': totalBelanja,
+    'status delivery': statusDelivery,
+    'daftar': daftarBelanja
+  };
+
+  print(dataECommerce);
+}
