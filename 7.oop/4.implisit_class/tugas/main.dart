51143a06f6dd6baeb4550468d17f10e4a31ea37a
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-07-30 14:08:37 +0700

    1

--- 7.oop/4.implisit_class/tugas/main.dart
+++ 7.oop/4.implisit_class/tugas/main.dart
@@ -1,20 +1,35 @@
+import 'animal.dart';
 import 'cat.dart';
 import 'dog.dart';
 import 'fish.dart';
 
 void main(List<String> args) {
-  Cat animal1 = Cat('snow', 2, 'white', 'blue');
+  Animal animal1 = Cat('snow', 2, 'white', 'blue');
   animal1.eat();
-  animal1.meow();
-  animal1.ciri();
 
-  Dog animal2 = Dog('Muso', 2, 'Brown', 'Balok');
+  Cat cat1 = Cat("kitty", 12, "Blue", "red");
+  cat1.meow();
+
+  Animal animal2 = Dog('Muso', 2, 'Brown', 'Balok');
+  Dog dog1 = Dog("dor", 12, "black", "segi");
+  dog1.woof();
+
+  Animal animal3 = Fish('Dory', 2, 'Blue', 'Panjang');
+  Fish fish1 = Fish("kiw", 12, "Blue", "set");
+  fish1.swim();
+
+  Cat cat = Cat('snow', 2, 'white', 'blue');
+  animal1.eat();
+  cat.meow();
+  cat.ciri();
+
+  Dog dog = Dog('Muso', 2, 'Brown', 'Balok');
   animal2.eat();
-  animal2.woof();
-  animal2.ciri();
+  dog.woof();
+  dog.ciri();
 
-  Fish animal3 = Fish('Dory', 2, 'Blue', 'Panjang');
+  Fish fish = Fish('Dory', 2, 'Blue', 'Panjang');
   animal3.eat();
-  animal3.swim();
-  animal3.ciri();
+  fish.swim();
+  fish.ciri();
 }
